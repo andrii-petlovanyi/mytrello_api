3b9d252a6f5bca0faf405c6ac97ea72b3a6d11bb
Andrii Petlovanyi <33178699+andrii-petlovanyi@users.noreply.github.com>
2023-02-22 08:53:29 +0200

    added(start pack): added main folders and files for work

---
+++ helpers/errors.js
@@ -0,0 +1,29 @@
+class CustomError extends Error {
+  constructor(message) {
+    super(message);
+    this.status = 400;
+  }
+}
+
+class AccessDeniedError extends CustomError {
+  constructor(message) {
+    super(message);
+    this.status = 403;
+  }
+}
+
+class NotAuthorizedError extends CustomError {
+  constructor(message) {
+    super(message);
+    this.status = 401;
+  }
+}
+
+class ConflictError extends CustomError {
+  constructor(message) {
+    super(message);
+    this.status = 409;
+  }
+}
+
+export { CustomError, NotAuthorizedError, ConflictError, AccessDeniedError };
---
+++ middlewares/errorsHandler.js
@@ -0,0 +1,10 @@
+import { CustomError } from './index.js';
+
+const errorsHandler = (error, req, res, next) => {
+  if (error instanceof CustomError) {
+    return res.status(error.status).json({ message: error.message });
+  }
+  res.status(500).json({ message: error.message });
+};
+
+export { errorsHandler };
