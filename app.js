a2ee9f644a39e4d4850342b69ca4381e63ddd094
Andrii Petlovanyi <33178699+andrii-petlovanyi@users.noreply.github.com>
2023-02-24 14:03:54 +0200

    fixed(404): fixed 404 in some behavior

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use("/api/cards", cardsRouter);
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 app.use(express.static(path.join(__dirname, "build")));
-app.get("/*", function (req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
